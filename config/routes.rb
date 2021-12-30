3b39eadec6b3d64318ac67f2aa651529ea5b4541
kazuki <kazuki@MacBookPro.local>
2021-12-31 05:58:14 +0900

    show.edit.update.destroyアクションを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   get 'tasks/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
+
 
 end
